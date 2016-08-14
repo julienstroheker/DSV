05387e77def96e23a7be99d38dfe1afb62ea38db
Julien STROHEKER <Julien@MacBook-Air-de-Julien.local>
2016-08-14 16:33:25 -0400

    Activing remote Docker Remote API

--- scripts/node.sh
+++ scripts/node.sh
@@ -4,5 +4,5 @@
 ### https://github.com/julienstroheker/DSV.git ###
 ##################################################
 
-TOKEN=`cat /vagrant/worker_token`
+TOKEN=`cat /vagrant/wtoken`
 docker swarm join --token=$TOKEN 10.0.0.10:2377
